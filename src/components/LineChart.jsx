85eb1c96d6fe588a76f885713c18c4650881ca7b
engWafula <wafulaallan5@gmail.com>
2022-01-28 17:47:35 +0300

    more  improvements

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,19 +1,22 @@
+
 import React from 'react';
-import {Line} from "react-chartjs-2"
+import { Line } from 'react-chartjs-2';
 import { Col, Row, Typography } from 'antd';
 
 const { Title } = Typography;
 
-export default function LineChart({coinHistory,coinName,currentPrice}) {
-      const coinPrice = [];
+const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  const coinPrice = [];
   const coinTimestamp = [];
 
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
     coinPrice.push(coinHistory?.data?.history[i].price);
-    coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
   }
 
-    const data = {
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
+  }
+  const data = {
     labels: coinTimestamp,
     datasets: [
       {
@@ -26,7 +29,7 @@ export default function LineChart({coinHistory,coinName,currentPrice}) {
     ],
   };
 
-   const options = {
+  const options = {
     scales: {
       yAxes: [
         {
@@ -38,9 +41,8 @@ export default function LineChart({coinHistory,coinName,currentPrice}) {
     },
   };
 
-
-  return(
-       <>
+  return (
+    <>
       <Row className="chart-header">
         <Title level={2} className="chart-title">{coinName} Price Chart </Title>
         <Col className="price-container">
@@ -48,7 +50,9 @@ export default function LineChart({coinHistory,coinName,currentPrice}) {
           <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
         </Col>
       </Row>
-    <Line data={data} options={options} /> 
+      <Line data={data} options={options} />
     </>
-  )
-}
+  );
+};
+
+export default LineChart;
--- src/services/CryptoApi.js
+++ src/services/CryptoApi.js
@@ -17,14 +17,11 @@ export const cryptoApi = createApi({
           query:(count)=>createRequest(`/coins?limit=${count}`)
       }),
        getCryptoDetails: builder.query({
-      query: (coinId) => createRequest(`/coin/${coinId}`),
-
-      
-    getCryptoHistory: builder.query({
-      query: ( {coinId, timeperiod} ) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
-    }),
-
-    }),
+      query: (coinId) => createRequest(`/coin/${coinId}`)
+}),
+getCryptoHistory: builder.query({
+  query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+}),
     })//This  arrow  function  instantly  returns  an  object
 })//This arrow function  instantly  returns  an  object
 
